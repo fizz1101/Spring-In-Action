747e2541b2f0928186a8c65419b40c125d6d10ae
fizz1101 <546094436@qq.com>
2017-11-30 11:38:09 +0800

    .

--- src/main/java/com/fizz/spring/part1/KnightMain.java
+++ src/main/java/com/fizz/spring/part1/KnightMain.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class KnightMain {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config/knight.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config/part1/knight.xml");
         AnnotationConfigApplicationContext context1 = new AnnotationConfigApplicationContext(com.fizz.spring.part1.config.KnightConfig.class);
         Knight knight = context.getBean("knight", Knight.class);
         knight.embarkOnQuest();
