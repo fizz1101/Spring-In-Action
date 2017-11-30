48c5686887df0c931c23fb62e8570c59f136306f
fizz1101 <546094436@qq.com>
2017-11-30 15:21:45 +0800

    part2 spring DI auto

---
+++ src/main/java/com/fizz/spring/part2/BlackDisc.java
@@ -0,0 +1,28 @@
+package com.fizz.spring.part2;
+
+import java.util.List;
+
+public class BlackDisc implements CompactDisc {
+
+    private String title;
+    private String artist;
+    private List<String> tracks;
+
+    public BlackDisc(String title, String artist, List<String> tracks) {
+        this.title = title;
+        this.artist = artist;
+        this.tracks = tracks;
+    }
+
+    public void play() {
+        System.out.println("Playing " + title + " by " + artist);
+        if (tracks != null) {
+            for (String track : tracks) {
+                System.out.println("-Track:" + track);
+            }
+        } else {
+            System.out.println("该磁带未载入歌曲");
+        }
+    }
+
+}
